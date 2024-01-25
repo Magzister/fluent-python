631a68d7e104080c77468a20fdbb4fad99fe88fb
Daniil Kulbatski <daniil.kulbackiy@gmail.com>
2024-01-25 12:10:43 +0100

    sqlalchemy tutorial

--- sql_alchemy/insert_core.py
+++ sql_alchemy/insert_core.py
@@ -1,25 +1,11 @@
-from sqlalchemy import text
+from sqlalchemy import insert
 
 from db import engine
+from create_table_core import user_table
 
 
+stmt = insert(user_table).values(name="spongebob", fullname="Spongebob Squarepants")
+print(stmt)
 with engine.connect() as conn:
-    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
-    conn.execute(
-        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
-        [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
-    )
-    conn.commit()
-
-with engine.begin() as conn:
-    conn.execute(
-        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
-        [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
-    )
-
-with engine.connect() as conn:
-    conn.execute(
-        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
-        [{"x": 11, "y": 12}, {"x": 13, "y": 14}],
-    )
+    result = conn.execute(stmt)
     conn.commit()
